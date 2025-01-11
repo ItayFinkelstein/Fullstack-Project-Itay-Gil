65b2424e9a95c5e80dec3d8229e198f9a0ae83f2
Itay <itay.finkelstein@gmail.com>
2025-01-11 18:37:12 +0200

    created base controller, added comment model for mongoose and adjusted the demo requests accordinly

--- controllers/comment.ts
+++ controllers/comment.ts
@@ -1,31 +1,7 @@
 import { Request, Response } from 'express';
+import commentModel from "../models/comment";
+import BaseController from "./base_controller";
 
-const getComments = (req: Request, res: Response): void => {
-    if (req.query.id !== undefined) {
-        res.send(`find all comments related to ${req.query.id}`);
-    } else {
-        res.send('get all comments');
-    }
-}
+const commentsController = new BaseController(commentModel);
 
-const getCommentByPostId = (req: Request, res: Response): void => {
-    res.send(`get comment of post id : ${req.params.id}`);
-}
-
-const getCommentByOwnerId = (req: Request, res: Response): void => {
-    res.send(`get comment of owner id : ${req.params.id}`);
-}
-
-const postComment = (req: Request, res: Response): void => {
-    res.send('post comment : ' + JSON.stringify(req.body));
-}
-
-const putComment = (req: Request, res: Response): void => {
-    res.send('put comment : ' + JSON.stringify(req.body));
-}
-
-const deleteComment = (req: Request, res: Response): void => {
-    res.send('delete comment : ' + req.params.id);
-}
-
-export { getComments, getCommentByPostId, getCommentByOwnerId, postComment, putComment, deleteComment };
+export default commentsController;
--- routes/comment_routes.ts
+++ routes/comment_routes.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import { getComments, getCommentByPostId, getCommentByOwnerId, postComment, putComment, deleteComment } from '../controllers/comment';
+import commentsController from '../controllers/comment';
 
-const commentRouter = Router();
+const commentsRouter = Router();
 
-commentRouter.get('/', getComments);
-commentRouter.get('/:id', getCommentByPostId);
-commentRouter.get('/owner/:id', getCommentByOwnerId);
-commentRouter.post('/', postComment);
-commentRouter.put('/:id', putComment);
-commentRouter.delete('/:id', deleteComment);
+commentsRouter.get("/", commentsController.getAll.bind(commentsController));
+commentsRouter.get("/:id", (req, res) => { commentsController.getById(req, res) });
+commentsRouter.post("/", commentsController.createItem.bind(commentsController));
+commentsRouter.put("/:id", (req, res) => { commentsController.updateItemById(req, res) });
+commentsRouter.delete("/:id", (req, res) => { commentsController.deleteItemById(req, res) });
 
-export default commentRouter;
+export default commentsRouter;
