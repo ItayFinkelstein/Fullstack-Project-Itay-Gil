5840e79bfb3a0c8c6eb269e1251eba7ecf271baf
Gil Abramovich <gilgil.abramovich@gmail.com>
2025-01-11 18:02:03 +0200

    Post controller, added routes. Checked in requests.rest.

--- controllers/post.js
+++ controllers/post.js
@@ -2,4 +2,20 @@ const getPosts = async(req, res, next) => {
     res.send('get all posts')
 }
 
-module.exports = {getPosts};
+const getPostById = (req, res) => {
+    res.send(`get post of id : ${req.params.id}`);
+}
+
+const savePost = (req, res) => {
+    res.send('save post : ' + JSON.stringify(req.body));
+}
+
+const updatePost = (req, res) => {
+    res.send('put post : ' + JSON.stringify(req.body));
+}
+
+const deletePost = (req, res) => {
+    res.send('delete post : ' + req.params.id);
+}
+
+module.exports = {getPosts, getPostById, savePost, updatePost, deletePost};
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const postController = require('../controllers/post')
 
-const Post = require('../controllers/post');
+router.get('/', postController.getPosts);
+router.get('/:id', postController.getPostById);
 
-router.get('/', Post.getPosts);
+router.post('/', postController.savePost);
+
+router.put('/:id', postController.updatePost);
+
+router.delete('/:id', postController.deletePost);
 
 module.exports = router;
