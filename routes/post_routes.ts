7ae75b73c752c497b4e5160fa22ad6cf95dcba4b
Gil Abramovich <gilgil.abramovich@gmail.com>
2025-01-22 21:13:05 +0200

    Add swagger for the basis of the page, and create the swagger documentation for the post routes.
    Having problem of needing different ports or it says "port is already in use", need to fix that because it seems to not show the routes in swagger, maybe because of this.

--- routes/post_routes.ts
+++ routes/post_routes.ts
@@ -3,13 +3,145 @@ import postsController from '../controllers/post_controller';
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - title
+ *         - content
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The post title
+ *         content:
+ *           type: string
+ *           description: The post content
+ *       example:
+ *         title: 'My First Post'
+ *         content: 'This is the content of my first post'
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Returns a list of all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: The list of posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
 router.get('/', postsController.getAll.bind(postsController));
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Get a post by ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The post ID
+ *     responses:
+ *       200:
+ *         description: The post description by ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: Post not found
+ */
 router.get('/:id', (req, res) => { postsController.getById(req, res) });
 
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       201:
+ *         description: The post was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       500:
+ *         description: Some server error
+ */
 router.post('/', postsController.createItem.bind(postsController));
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Update a post by ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The post ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: The post was successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: Post not found
+ *       500:
+ *         description: Some server error
+ */
 router.put('/:id', (req, res) => { postsController.updateItemById(req, res) });
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Delete a post by ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The post ID
+ *     responses:
+ *       200:
+ *         description: The post was successfully deleted
+ *       404:
+ *         description: Post not found
+ */
 router.delete('/:id', (req, res) => { postsController.deleteItemById(req, res) });
 
 export default router;
--- server.ts
+++ server.ts
@@ -5,6 +5,9 @@ import indexRouter from './routes/index';
 import postRouter from './routes/post_routes';
 import commentRouter from './routes/comment_routes';
 import mongoose from "mongoose";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+
 
 dotenv.config();
 
@@ -25,6 +28,25 @@ const initApp = async (): Promise<Express> => {
             reject(new Error("DB_CONNECTION is not defined in .env file"));
         } else {
             await mongoose.connect(process.env.DB_CONNECTION);
+            if (process.env.NODE_ENV == "development") {
+                const options = {
+                definition: {
+                openapi: "3.0.0",
+                info: {
+                title: "Fullstack project",
+                version: "1.0.0",
+                description: "Project of posts and comments, with user authentication",
+                },
+                servers: [{url: "http://localhost:3000"},],
+                },
+                apis: ["./src/routes/*.ts"],
+                };
+                const specs = swaggerJsDoc(options);
+                app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+                console.log("set up swagger")
+            }
+        
+
             resolve(app);
         }
     });
